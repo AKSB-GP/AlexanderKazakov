c2dda7d7537e1618afa9a9766de93a07543a1473
RussianKazakov <35559511+betedig@users.noreply.github.com>
2024-06-10 21:26:34 +0200

    working 3d scene

--- src/R3F/Scene.jsx
+++ src/R3F/Scene.jsx
@@ -5,18 +5,22 @@ import { Canvas } from '@react-three/fiber'
 
 
 
-const Scene = () =>{
-        <div className="bg-red-300">
-<Canvas/>
-  <ambientLight intensity={0.1} />
-  <directionalLight color="red" position={[0, 0, 5]} />
-  <mesh>
-    <boxGeometry />
-    <meshStandardMaterial />
-  </mesh>
-        
-        </div>
+const Scene = () => {
+  return (
+    //ändras diven för att täcka hela
+    <div className="bg-yellow-300">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="hotpink" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  )
 
-    
+
+
 }
 export default Scene
