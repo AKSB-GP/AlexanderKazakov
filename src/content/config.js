0a03f2101652025fcb7cf3b327ca621c98151f35
RussianKazakov <35559511+betedig@users.noreply.github.com>
2024-06-10 13:07:17 +0200

    started on slugs

---
+++ src/content/config.js
@@ -0,0 +1,29 @@
+//zod and astro collection
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const WorkCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    jobdescription: z.string(),
+    date: z.number().positive(),
+    company: z.string(),
+  }),
+});
+
+const ProjectsCollection = defineCollection({
+    type: 'content',schema: z.object({
+        title: z.string(),
+        projectdescription: z.string(),
+        link: z.string(),
+        image: z.string().optional(),
+    }),
+        
+});
+
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'Work': WorkCollection,'Projects': ProjectsCollection,
+};
