15192cbabb27dbd15d0c99a5e22062e326b68d56
RussianKazakov <35559511+betedig@users.noreply.github.com>
2024-06-29 11:22:14 +0200

    radial menu adjustments

--- src/components/Typewriter.jsx
+++ src/components/Typewriter.jsx
@@ -6,7 +6,7 @@ const Typewriter = () => {
   return (
      //typed speed in miliseconds
     <>   
-    <div class ="flex flex-col inline items-center">
+    <div class ="flex flex-col inline items-center ">
     <div class=" text-7xl ">
       <h1>ALEXANDER KAZAKOV</h1>
       </div >
